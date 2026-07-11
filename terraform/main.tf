eee2cf75bdc2c00270b57ab88c07d81edd261d4d
Regdeg90 <reubengedge@gmail.com>
2026-07-11 19:24:40 +0100

    api gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -266,11 +266,6 @@ resource "aws_lambda_function" "view_counter" {
 
 resource "aws_sns_topic" "cv_updates" {
   name = "${var.environment}-cv-updates"
-  environment {
-    variables = {
-      SNS_TOPIC_ARN = aws_sns_topic.cv_updates.arn
-    }
-  }
 }
 
 resource "aws_iam_role_policy" "subscribe_lambda_sns" {
@@ -290,4 +285,53 @@ resource "aws_iam_role_policy" "subscribe_lambda_sns" {
       }
     ]
   })
+}
+
+resource "aws_lambda_function" "subscribe" {
+  function_name = "${var.environment}-cv-subscribe"
+
+  role    = aws_iam_role.lambda_exec.arn
+  runtime = "python3.12"
+  handler = "lambda_function.lambda_handler"
+
+  filename = "../ui/lambda/subscribe/lambda.zip"
+
+  source_code_hash = filebase64sha256(
+    "../ui/lambda/subscribe/lambda.zip"
+  )
+
+  timeout     = 10
+  memory_size = 128
+
+  environment {
+    variables = {
+      SNS_TOPIC_ARN = aws_sns_topic.cv_updates.arn
+    }
+  }
+}
+
+resource "aws_apigatewayv2_integration" "subscribe" {
+  api_id = aws_apigatewayv2_api.cv_api.id
+
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.subscribe.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "subscribe" {
+  api_id = aws_apigatewayv2_api.cv_api.id
+
+  route_key = "POST /subscribe"
+
+  target = "integrations/${aws_apigatewayv2_integration.subscribe.id}"
+}
+
+resource "aws_lambda_permission" "subscribe_api_gateway" {
+  statement_id  = "AllowSubscribeAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.subscribe.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.cv_api.execution_arn}/*/POST/subscribe"
 }
